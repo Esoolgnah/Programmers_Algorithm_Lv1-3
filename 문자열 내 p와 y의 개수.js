b06e248875efdae021c8709bea556e63602bc623
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-08-03 23:40:56 +0900

    문제 풀이, Advanced Challege 풀어보기

--- 문자열 내 p와 y의 개수.js
+++ 문자열 내 p와 y의 개수.js
@@ -1,11 +1,16 @@
 function solution(s){
-    var answer = true;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
-    return answer;
-}
+    s = s.toLowerCase();
+    let sCount = 0, yCount = 0;
+      for (let i = 0; i < s.length; i++){
+        if (s[i] === 'p'){
+          sCount++;
+        }
+        if (s[i] === 'y'){
+          yCount++;
+        }
+      }
+    return sCount === yCount;
+    }
 
 // 문제 설명
 // 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
@@ -25,3 +30,4 @@ function solution(s){
 
 // 입출력 예 #2
 // 'p'의 개수 1개, 'y'의 개수 2개로 다르므로 false를 return 합니다.
+//TODO: split('')을 이용해 return 한 줄만으로 끝내보기 (Advanced Challenge)
