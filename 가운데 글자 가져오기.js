e70a25257aec8aa0dd3c732526763ac19882e5ca
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-08-06 17:19:35 +0900

    문제 풀이, Advanced Challenge 풀어보기

--- 가운데 글자 가져오기.js
+++ 가운데 글자 가져오기.js
@@ -1,11 +1,17 @@
 function solution(s) {
-    if ((s.length/2) % 2 === 0){
-      return String(s[(s.length/2)-1] + s[s.length/2])
+  let sIndex = 0;
+  if (s.length === 2){
+    return s;
+  }else{
+    if (s.length % 2 === 0){
+      sIndex = Math.round(s.length/2)-1;
+      return s.substring(sIndex,sIndex+2);
     }else{
-      return String((s[Math.round(s.length/2)-1]))
+      sIndex = Math.round(s.length/2)-1;
+      return s[sIndex];
     }
   }
-
+  }
 //   문제 설명
 // 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
 
@@ -15,3 +21,4 @@ function solution(s) {
 // s	return
 // "abcde"	"c"
 // "qwer"	"we"
+//TODO: Math.ceil 을 이용하여 풀어보기
