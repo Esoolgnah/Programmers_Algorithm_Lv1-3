2c3125d02def2379d1ee811116eaa3eee8131f30
Hangloose <85841690+Esoolgnah@users.noreply.github.com>
2021-12-08 23:56:49 +0900

    문제 풀이

--- 부족한 금액 계산하기.js
+++ 부족한 금액 계산하기.js
@@ -1,6 +1,9 @@
 function solution(price, money, count) {
-    for (let i = 0; i < count; i++){
-    }
+    let sum = 0;
+    for (let i = 1; i <= count; i++) sum += price * i;
+    const result = money - sum;
+    if (result > 0) return 0
+    else return Math.abs(result);
   }
 // 문제 설명
 // 새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다. 이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다. 즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
