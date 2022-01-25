2620d3c011d27b28597e860c315cfa31cbdd6541
Jae Hyeok Jeong <85841690+Esoolgnah@users.noreply.github.com>
2022-01-26 02:49:26 +0900

    문제 풀이

--- 크레인 인형뽑기게임.js
+++ 크레인 인형뽑기게임.js
@@ -1,4 +1,20 @@
 function solution(board, moves) {
-  var answer = 0;
-  return answer;
+  const answer = [];
+  let count = 0;
+  for(let i=0 ; i < moves.length ; i++){
+    for(let row=0 ; row < board.length ; row++){
+      const col = moves[i]-1;
+      if(board[row][col] > 0){
+        if(answer[answer.length - 1] === board[row][col]){
+          answer.pop();
+          count += 2;
+        }else{
+          answer.push(board[row][col]);
+        }
+        board[row][col] = 0;
+        break;
+      }
+    }
+  }
+  return count;
 }
