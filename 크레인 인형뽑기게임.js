1d54183e085afede65f81c7947e0da749110c754
Jae Hyeok Jeong <85841690+Esoolgnah@users.noreply.github.com>
2022-01-26 11:07:26 +0900

    문제 재풀이

--- 크레인 인형뽑기게임.js
+++ 크레인 인형뽑기게임.js
@@ -1,20 +1,27 @@
 function solution(board, moves) {
-  const answer = [];
-  let count = 0;
-  for(let i=0 ; i < moves.length ; i++){
-    for(let row=0 ; row < board.length ; row++){
-      const col = moves[i]-1;
-      if(board[row][col] > 0){
-        if(answer[answer.length - 1] === board[row][col]){
-          answer.pop();
-          count += 2;
+  let answer = 0, basket = [];
+  moves.forEach((move)=>{
+    //moves만큼 반복문
+    const y = move-1;
+    //y축의 index에 맞추기 위해 1을 빼준다.
+    for (let x = 0; x < board.length; x++){
+      if (board[x][y] > 0){
+      // 인형이 있을 때
+        if (basket[basket.length-1] === board[x][y]){
+        // 이전에 바구니에 담긴 인형과 동일하면
+          basket.pop();
+          answer += 2;
+          // 바구니를 비우고 점수 추가
         }else{
-          answer.push(board[row][col]);
+          basket.push(board[x][y]);
+          //바구니에 추가
         }
-        board[row][col] = 0;
+        board[x][y] = 0;
+        // 집은 인형 삭제
         break;
+        //반복문 끝내고 다음 move로
       }
     }
-  }
-  return count;
+  })
+  return answer;
 }
