f579bc3bdf75900435d9c0ccbe0b631a016c7d97
Jae Hyeok Jeong <85841690+Esoolgnah@users.noreply.github.com>
2022-02-01 23:56:35 +0900

    문제 풀이중

--- 신고 결과 받기.js
+++ 신고 결과 받기.js
@@ -1,3 +1,15 @@
 function solution(id_list, report, k) {
-
+  let idList = {};
+  for (let i = 0; i < id_list.length; i++){
+    idList[id_list[i]] = 0;
+  }
+  for (let j = 0; j < report.length; j++){
+    const splitReport = report[j].split(' ');
+    const reporter = splitReport[0];
+    const target = splitReport[1];
+    if (idList[reporter] === 0){
+      idList[target] += 1;
+    }
+  }
+  return idList;
 }
