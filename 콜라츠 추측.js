61c7d1227e1e652fe2659e1a02cfa0fdd1a3226d
Jae Hyeok Jeong <85841690+Esoolgnah@users.noreply.github.com>
2022-02-04 13:07:08 +0900

    문제 풀이 & Advanced Challenge 풀이

--- 콜라츠 추측.js
+++ 콜라츠 추측.js
@@ -1,18 +1,27 @@
+// function solution(num) {
+//     let count = 0;
+//     while (num > 1){
+//       count++;
+//       if (count === 500){
+//         count = -1
+//         break;
+//       }
+//       if (num % 2 === 0){
+//         num = num / 2;
+//       }else if (num % 2 !== 0){
+//         num = (num * 3) + 1;
+//       }
+//     }
+//     return count;
+// }
+
 function solution(num) {
-    let count = 0;
-    while (num > 1){
-      count++;
-      if (count === 500){
-        count = -1
-        break;
-      }
-      if (num % 2 === 0){
-        num = num / 2;
-      }else if (num % 2 !== 0){
-        num = (num * 3) + 1;
-      }
-    }
-    return count;
+  let count = 0;
+  while (num > 1 || count === 500){
+    count++;
+    num % 2 === 0 ? num /= 2 : num % 2 !== 0 ? num = (num * 3) + 1: null;
+  }
+  return count < 500 ? count : -1;
 }
 
 // 문제 설명
