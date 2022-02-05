c07aa09ed53979a56e315071db731884c918ea49
Jae Hyeok Jeong <85841690+Esoolgnah@users.noreply.github.com>
2022-02-05 23:30:26 +0900

    문제 풀이 & Advanced Challege 풀이

--- 짝수와 홀수.js
+++ 짝수와 홀수.js
@@ -1,9 +1,12 @@
+// function solution(num) {
+//     if (num % 2 === 0 || num === 0){
+//       return "Even";
+//     }else{
+//       return "Odd";
+//     }
+// }
 function solution(num) {
-    if (num % 2 === 0 || num === 0){
-      return "Even";
-    }else{
-      return "Odd";
-    }
+  return num % 2 === 0 || num === 0 ? 'Even' : 'Odd';
 }
 // 문제 설명
 // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
