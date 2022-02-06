da6ed1a5e88b6aa92bf2e56cbc2b82b449c0de3d
Jae Hyeok Jeong <85841690+Esoolgnah@users.noreply.github.com>
2022-02-06 15:33:16 +0900

    문제 풀이, Advanced Challenge 풀이

--- 정수 제곱근 판별.js
+++ 정수 제곱근 판별.js
@@ -1,11 +1,17 @@
+// function solution(n) {
+//   let x = Math.sqrt(n);
+//   if (x % 1 === 0){
+//     return (x + 1) ** 2;
+//   }else{
+//     return -1;
+//   }
+// }
+
+//Advanced Challenge
 function solution(n) {
-  let x = Math.sqrt(n);
-  if (x % 1 === 0){
-    return (x + 1) ** 2;
-  }else{
-    return -1;
-  }
+  return Math.sqrt(n) % 1 === 0 ? (Math.sqrt(n) + 1) ** 2 : -1;
 }
+
 // 문제 설명
 // 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
 // n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
@@ -22,3 +28,4 @@ function solution(n) {
 
 // 입출력 예#2
 // 3은 양의 정수의 제곱이 아니므로, -1을 리턴합니다.
+//TODO: 삼항연산자를 사용하여 풀어보자. (Advanced Challenge)
