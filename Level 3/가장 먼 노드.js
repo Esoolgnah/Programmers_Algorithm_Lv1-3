6fc752c0106ca7d08f6d3f6abdc17f6e3b8a627c
Esoolgnah <nezcoreen@gmail.com>
2022-05-15 23:20:01 +0900

    Edit: Lv3. 가장 먼 노드 문제풀이

--- Level 3/가장 먼 노드.js
+++ Level 3/가장 먼 노드.js
@@ -1,6 +1,32 @@
 function solution(n, edge) {
-  var answer = 0;
-  return answer;
+  let answer = 0;
+  const bfs = (arr,start,end) =>{
+    let visited = new Array(end + 1);
+    let level = new Array(end + 1);
+    let queue = [start];
+    level[0] = 0;
+    level[start] = 0;
+    visited[start] = true;
+    let lev;
+    while(queue.length){
+      var node = queue.shift()
+      lev = level[node] + 1;
+      for(let edge of arr){
+        if(edge[0]==node && visited[edge[1]]==undefined) {
+          queue.push(edge[1]);
+          visited[edge[1]] = true;
+          level[edge[1]] = lev;
+        }
+        else if(edge[1]==node && visited[edge[0]]==undefined) {
+          queue.push(edge[0]);
+          visited[edge[0]]=true;
+          level[edge[0]] = lev;
+        }
+      }
+    }
+  return level.filter((el)=> {return el === lev-1}).length;
+  }    
+  return bfs(edge,1,n);
 }
 
 // 문제 설명
